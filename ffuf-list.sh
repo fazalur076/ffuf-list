0927609fd36fff2c00411cf527df217e3026c521
Fazal <72785775+fazalurrahman076@users.noreply.github.com>
2023-06-08 19:00:48 +0530

    Update ffuf-list.sh

--- ffuf-list.sh
+++ ffuf-list.sh
@@ -2,7 +2,7 @@
 
 mkdir .ffuf
 for line in $(cat $1) ; do
-    ffuf -u "$line/FUZZ" -w <wordlist> -mc 200 -of csv -o .ffuf/$RANDOM.txt
+    ffuf -u "$line/FUZZ" -w $2 -mc 200 -of csv -o .ffuf/$RANDOM.txt
 done
 
 cat .ffuf/* | sort -u > ffuf.csv
